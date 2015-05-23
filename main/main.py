3f355423b3a6c78fb80739e4d78abde06e137d46
dnlm92 <dnlm92@me.com>
2015-05-23 01:00:12 -0500

    clean up

--- main/main.py
+++ main/main.py
@@ -26,8 +26,6 @@ api_v1 = helpers.Api(app, prefix='/api/v1')
 
 import api.v1
 
-from control import message
-
 if config.DEVELOPMENT:
   from werkzeug import debug
   app.wsgi_app = debug.DebuggedApplication(app.wsgi_app, evalex=True)
--- main/model/__init__.py
+++ main/model/__init__.py
@@ -4,4 +4,3 @@ from .base import Base
 from .config_auth import ConfigAuth
 from .config import Config
 from .user import User
-from .message import Message
